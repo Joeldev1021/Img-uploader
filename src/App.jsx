23bcd3847f48da31ceeb4c27d7d2c218708063da
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-15 18:16:50 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ImgUploader from './components/imgUploader/ImgUploader'
+import './App.scss';
+function App() {
+  return (
+    <div className="App">
+       <ImgUploader/> 
+    </div>
+  );
+}
+
+export default App;
