ff334595794842631cbce6ab6cb989087b96b3a2
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-17 18:12:34 -0500

    feat: create hook useDragAndDrop

---
+++ src/hooks/useDragAndDrop.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const useDragAndDrop = () => {
+    const [dragOver, setDragOver] = useState(false)
+    const [fileDropError, setFileDropError] = useState("")
+
+    const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setDragOver(true)
+    }
+    const onDragLeave = () => setDragOver(false) 
+
+    return {
+        dragOver,
+        setDragOver,
+        onDragOver,
+        onDragLeave,
+        fileDropError,
+        setFileDropError
+    }
+}
+
+export default useDragAndDrop
