e334a344fdc3269e9faf5dd8f4bf2570d8ca1948
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-18 18:12:05 -0500

    feat: add buttonUpload

--- src/components/imgUploader/ImgUploader.jsx
+++ src/components/imgUploader/ImgUploader.jsx
@@ -1,47 +1,22 @@
 import React, { useState } from "react";
-import FileDragAndDrop from "../FileDragAndDrop/fileDragAndDrop/FileDragAndDrop";
+import useFirebaseUpload from "../../hooks/useFirebaseUpload";
+import ButtonUpload from "../ButtonUpload/ButtonUpload";
+import DragAndDrop from "../DragAndDrop/DragAndDrop";
 import "./index.scss"
+
 const ImgUploader = () => {
-  const [imgUrl, setImgUrl] = useState(null);
-  const [progresspercent, setProgresspercent] = useState(0);
+  const [file, setFile] = useState(null)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const file = e.target[0].files[0];
-    if (!file) return;
+ const {imgUrl, progresspercent} = useFirebaseUpload({file})
 
-    const storageRef = ref(storage, `files/${file.name}`);
-    const uploadImg = uploadBytesResumable(storageRef, file);
-
-    uploadImg.on(
-      "state_changed",
-      (snapshot) => {
-        const progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-        setProgresspercent(progress);
-      },
-      (error) => {
-        alert(error);
-      },
-      () => {
-        getDownloadURL(uploadImg.snapshot.ref).then((downloadURL) => {
-          setImgUrl(downloadURL);
-        });
-      }
-    );
-  };
 
   return (
     <div className="container">
       <p className="title">Upload your image</p>
       <p className="text">File should be Jpeg, Png,...</p>
-      <FileDragAndDrop/>
+      <DragAndDrop/>
       <span className="or">or</span>
-      <form >
-        <input type="file" id="file" className="custom-file" name="image"  />
-       <label htmlFor="file" className="custom-file-btn">Choose</label>
-      </form>
+      <ButtonUpload setFile={setFile}/>
      {progresspercent > 0 && <p>{progresspercent}%</p>}
       {imgUrl && <p>{imgUrl}</p>}
     </div>
