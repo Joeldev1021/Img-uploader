927edd96622a2e888000dfc772e77cefe1108475
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-23 18:19:28 -0500

    Image-uploader finish 🔥

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import ImgUploader from './components/imgUploader/ImgUploader'
 import './App.scss';
+import { useState } from 'react';
+import useFirebaseUpload from './hooks/useFirebaseUpload';
+import ImgSelection from './components/ImgSelection/ImgSelection';
+import Uploaded from './components/Uploading/Uploading';
+
 function App() {
+  const [file, setFile] = useState(null)
+
+ const {imgUrl, progressPercent} = useFirebaseUpload({file})
+
   return (
     <div className="App">
-       <ImgUploader/> 
+      {progressPercent > 0 ? <Uploaded imgUrl={imgUrl} progressPercent={progressPercent} /> : <ImgSelection setFile={setFile} />}
     </div>
   );
 }
