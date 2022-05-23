db9ea0557c086b8be116a2229e7b1aaaee6e7f06
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-23 18:15:26 -0500

    feat: img selection

--- src/components/ImgSelection/ImgSelection.jsx
+++ src/components/ImgSelection/ImgSelection.jsx
@@ -4,23 +4,17 @@ import ButtonUpload from "../ButtonUpload/ButtonUpload";
 import DragAndDrop from "../DragAndDrop/DragAndDrop";
 import "./index.scss"
 
-const ImgUploader = () => {
-  const [file, setFile] = useState(null)
-
- const {imgUrl, progresspercent} = useFirebaseUpload({file})
-
+const ImgSelection = ({setFile}) => {
 
   return (
     <div className="container">
       <p className="title">Upload your image</p>
       <p className="text">File should be Jpeg, Png,...</p>
-      <DragAndDrop/>
-      <span className="or">or</span>
+      <DragAndDrop setFile={setFile}/>
+      <span  className="or">or</span>
       <ButtonUpload setFile={setFile}/>
-     {progresspercent > 0 && <p>{progresspercent}%</p>}
-      {imgUrl && <p>{imgUrl}</p>}
-    </div>
+    </div> 
   );
 };
 
-export default ImgUploader;
+export default ImgSelection;
